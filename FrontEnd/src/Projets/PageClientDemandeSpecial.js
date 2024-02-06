58b3d5b949b1bb8b62453475f96c186e4f03bd51
Thower56 <112575794+Thower56@users.noreply.github.com>
2024-02-06 15:11:18 -0500

    cote client fini 95%

--- FrontEnd/src/Projets/PageClientDemandeSpecial.js
+++ FrontEnd/src/Projets/PageClientDemandeSpecial.js
@@ -1,41 +1,92 @@
 import {React, useEffect, useState} from "react";
 import 'bootstrap/dist/css/bootstrap.css';
-import { ListeDemandeSpecialeClient } from "../composants/ListeDemandeSpecialeClient";
+import { PiecesDemandeSpecialeClient } from "../composants/PiecesDemandeSpecialeClient";
 import { Button } from "react-bootstrap";
-import { Piece } from "../composants/Piece";
+import { ListePieces } from "../composants/ListePieces";
 
 
 export const PageClientDemandeSpecial = () => {
-    const [ListePieces, setListePieces] = useState([]);
-    const [repertoire, setRepertoire] = useState([]);
-
+    const [client, setClient] = useState('');
+    const [demandePieces, setListePieces] = useState([]);
+    const [Pieces, setPieces] = useState([]);
+    const [filtreTitre, setfiltreTitre] = useState('');
+    const [filtreArtiste, setfiltreArtiste] = useState('');
+    const [filtreCategorie, setfiltreCategorie] = useState('');
+    const [selectionTri, setselectionTri] = useState('trieParCategorie');
+    const [message, setMessage] = useState('');
     
     useEffect(() => {
-        getRepertoire();
-    }, [ListePieces]);
+        getListePieces();
+    }, [demandePieces,selectionTri, filtreTitre,filtreArtiste,filtreCategorie]);
 
-    const getRepertoire = async () => {
+    const getListePieces = async () => {
         try {
             const resultat = await fetch(`http://localhost:8000/api/pieces`);
             const body = await resultat.json();
-            setRepertoire(body);
+            const regexpTitre = new RegExp(filtreTitre, 'i');
+            const regexpArtiste = new RegExp(filtreArtiste, 'i');
+            const regexpCategorie = new RegExp(filtreCategorie, 'i');
+            const liste = body.filter((piece) => regexpTitre.test(piece.titre.toLowerCase()) && regexpArtiste.test(piece.artiste.toLowerCase()) && regexpCategorie.test(piece.categorie.toLowerCase()));
+            setPieces(liste);
         } catch (error) {
             console.log('Error:', error);
+        }
+    };
+
+    const postDemandeSpeciales = async () => {
+        try{
+            const requestOptions = {
+                method: 'POST',
+                body: JSON.stringify({ client: client, demandePieces: demandePieces }),
+                headers: { 'Content-Type': 'application/json' },
+            };
+
+            const resultat = await fetch(`http://localhost:8000/api/demandeSpeciale/ajouter`, requestOptions);
+            if(resultat.status === 200){setMessage("Ajouté avec succès!");}
+            else{setMessage("Erreur lors de l'ajout.");}
+        } catch (e) {
+            console.log('Error:', e);
         }
     }
 
     const DeletePiece = (id) => {
-        const newListePieces = ListePieces.filter((piece) => piece._id !== id);
+        const newListePieces = demandePieces.filter((piece) => piece._id !== id);
         setListePieces(newListePieces);
     }
 
     const AjouterPiece = (id) => {
-        var newListePieces = ListePieces;
-        const piece = repertoire.find((p) => p._id === id);
+        var newListePieces = demandePieces;
+        const piece = Pieces.find((p) => p._id === id);
         newListePieces.push(piece);
         newListePieces = newListePieces.filter((value ,index , array)=>array.findIndex(t=>(t._id === value._id)) === index);
         setListePieces(newListePieces);
-        console.log(ListePieces);
+        console.log(demandePieces);
+    }
+
+    const changerfiltreTitre = (e) => {
+        console.log(e.target.value);
+        setfiltreTitre(e.target.value);
+    }
+
+    const changerfiltreArtiste = (e) => {
+        console.log(e.target.value);
+        setfiltreArtiste(e.target.value);
+    }
+
+    const changerfiltreCategorie = (e) => {
+        console.log(e.target.value);
+        setfiltreCategorie(e.target.value);
+    }
+    
+    const changementDeTri = (e) => {
+        if(e.target.value == null){
+            setselectionTri(/%%/);
+        }
+        setselectionTri(e.target.value)
+    };
+
+    const changementDeNom = (e) => {
+        setClient(e.target.value);
     }
 
     return (
@@ -43,24 +94,42 @@ export const PageClientDemandeSpecial = () => {
             <h1>Demande Spéciale</h1>
             <div className="row">
                 <div className="col-2">
-                    <input type="text" className="form-control" placeholder="Votre nom" />
+                    <input type="text" className="form-control" placeholder="Votre nom"  onChange={changementDeNom}/>
                     
                 </div>
                 <div className="col-2">
-                    <Button variant="primary">Envoyer</Button>
+                    <Button variant="primary" onClick={postDemandeSpeciales}>Envoyer</Button>
+                    
                 </div>
+                <div className="col-2"><span>{message}</span></div>
             </div>
             <h2>Votre demande special:</h2>
-            <ListeDemandeSpecialeClient Pieces={ListePieces} DeletePiece={DeletePiece}/>
+            <PiecesDemandeSpecialeClient Pieces={demandePieces} DeletePiece={DeletePiece}/>
             <h2>Nos pieces:</h2>
-            <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gap: "20px" }}>
-            {repertoire.map((piece) => (
-                <div key={piece._id}>
-                    <Piece piece={piece}/>
-                    <button className="btn btn-primary" onClick={()=> AjouterPiece(piece._id)}>Ajouter</button>
-                </div>
-            ))}
+            <div>
+                <span>Trier par:</span>
             </div>
+            <div className='col-2'>
+                    <select id='selection' className='form-control' onChange={changementDeTri}>
+                        <option value="trieParCategorie">Categorie</option>
+                        <option value="trieParArtiste">Artiste</option>
+                        <option value="trieParTitre">Titre</option>
+                    </select>
+            </div>
+            <span>Recherche:</span>
+            <div className="row">
+                <div className='col-2'> 
+                    <input className='form-control' placeholder='Titre...' onChange={changerfiltreTitre}></input>
+                </div>
+                <div className='col-2'> 
+                <input className='form-control' placeholder='Artiste...' onChange={changerfiltreArtiste}></input>
+                </div>
+                <div className='col-2'> 
+                    <input className='form-control' placeholder='Categorie...' onChange={changerfiltreCategorie}></input>
+                </div>
+            </div>
+                
+                <ListePieces pieces={Pieces} value={selectionTri} button={AjouterPiece}/>
         </div>
     )
 };
