04e9b8c49cb034d5f9e86793e89890ee0cc02bf7
laurentfaggin <laurentfaggin@hotmail.fr>
2024-02-12 14:00:15 -0500

    bug Langages

--- FrontEnd/src/composants/Langages.js
+++ FrontEnd/src/composants/Langages.js
@@ -6,7 +6,7 @@ import { Form } from "react-bootstrap";
 export function Langages() {
     const { i18n } = useTranslation();
 
-    useEffect(() => i18n.changeLanguage("en"), [i18n]);
+    useEffect(() => i18n.changeLanguage("fr"), [i18n]);
 
     function changerLangue(langue) {
         i18n.changeLanguage(langue); 
@@ -15,10 +15,10 @@ export function Langages() {
         <Row className="mt-3">
             <Col xs={{span: 6, offset: 6}} sm={{span: 3, offset: 9}} lg={{span: 2, offset: 10}}>
                 <Form.Group as="select" onChange={(event) => changerLangue(event.target.value)}>
-                    <option value="fr>">Français</option>
+                    <option value="fr">Français</option>
                     <option value="en">English</option>
                 </Form.Group>
             </Col>
         </Row>
     );
-    }
+}
